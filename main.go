6a4f992aa1a97e0dbf8a8208006b09ca3cb110b4
nas <nas.silver.bullet@gmail.com>
2020-03-03 02:20:12 +0900

    :sparkles: Add tokenizer

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"unicode"
+)
+
 const HEAPSIZE = 10000000
 const NIL = 0
 
@@ -53,6 +60,45 @@ func (cs cells) setCDR(addr int, x int)        { cs[addr].cdr = x }
 func (cs cells) setTag(addr int, t tag)        { cs[addr].tag = t }
 func (cs cells) setName(addr int, name string) { heap[addr].name = name }
 
+type toktype int
+
+const (
+	_ toktype = iota
+	LPAREN
+	RPAREN
+	QUOTE
+	DOT
+	NUMBER
+	SYMBOL
+	OTHER
+)
+
+type backtrack int
+
+const (
+	_ backtrack = iota
+	GO
+	BACK
+)
+
+type token struct {
+	ch      rune
+	flag    backtrack
+	toktype toktype
+	buf     []rune
+}
+
+const BUFSIZE = 256
+
+var stok = token{flag: GO, toktype: OTHER}
+
+const (
+	EOL        = '\n'
+	TAB        = '\t'
+	SPACE      = ' '
+	NUL   rune = 0
+)
+
 func initcell() {
 	for addr := 0; addr < HEAPSIZE; addr++ {
 		heap = append(heap, cell{cdr: addr + 1})
@@ -89,6 +135,148 @@ func assocsym(sym int, val int) {
 	ep = cons(cons(sym, val), ep)
 }
 
+func gengetchar(txt string) func() rune {
+	txt += "\n"
+	i := 0
+	return func() rune {
+		i++
+		return rune(txt[i-1])
+	}
+}
+
+func getchar() rune {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return []rune(input)[0]
+}
+
+func numbertoken(buf []rune) bool {
+	if (buf[0] == '+') || (buf[0] == '-') {
+		buf = buf[1:]
+		if buf[0] == NUL {
+			return false
+		}
+	}
+
+	for _, c := range buf {
+		if c == NUL {
+			break
+		}
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
+
+func isalpha(c rune) bool {
+	if c < 'A' || c > 'z' {
+		return false
+	}
+	if c > 'Z' && c < 'a' {
+		return false
+	}
+	return true
+}
+
+func issymch(c rune) bool {
+	switch c {
+	case '!':
+	case '?':
+	case '+':
+	case '-':
+	case '*':
+	case '/':
+	case '=':
+	case '<':
+	case '>':
+		return true
+	}
+
+	return false
+}
+
+func symboltoken(buf []rune) bool {
+	if unicode.IsDigit(buf[0]) {
+		return false
+	}
+
+	for _, c := range buf {
+		if c == NUL {
+			break
+		}
+		if unicode.IsDigit(c) || isalpha(c) || issymch(c) {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func gettoken() {
+	if stok.flag == BACK {
+		stok.flag = GO
+		return
+	}
+
+	if stok.ch == ')' {
+		stok.toktype = RPAREN
+		stok.ch = NUL
+		return
+	}
+
+	if stok.ch == '(' {
+		stok.toktype = LPAREN
+		stok.ch = NUL
+		return
+	}
+
+	// sc := bufio.NewScanner(os.Stdin)
+	// sc.Scan()
+
+	var pos int
+	// getchar := gengetchar(sc.Text())
+
+	c := getchar()
+	for c == SPACE || c == EOL || c == TAB {
+		c = getchar()
+	}
+
+	switch c {
+	case '(':
+		stok.toktype = LPAREN
+	case ')':
+		stok.toktype = RPAREN
+	case '\'':
+		stok.toktype = QUOTE
+	case '.':
+		stok.toktype = DOT
+	default:
+		pos++
+		for c != EOL && pos < BUFSIZE && c != SPACE && c != '(' && c != ')' {
+			pos++
+			stok.buf = append(stok.buf, c)
+			c = getchar()
+		}
+
+		stok.buf = append(stok.buf, NUL)
+		stok.ch = c
+
+		if numbertoken(stok.buf) {
+			stok.toktype = NUMBER
+			break
+		}
+		if symboltoken(stok.buf) {
+			stok.toktype = SYMBOL
+			break
+		}
+		stok.toktype = OTHER
+	}
+}
+
 func main() {
 	initcell()
+	gettoken()
+	fmt.Printf("stok.buf: %v\n", stok.buf)
+	fmt.Printf("stok.toktype: %v\n", stok.toktype)
 }
